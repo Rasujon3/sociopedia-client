5973dbd8729598c73f15debdb0cfe96f8ef43b89
Rasujon3 <rasujon3@gmail.com>
2023-01-07 21:31:20 +0600

    navbar done

--- src/scenes/homePage/HomePage.jsx
+++ src/scenes/homePage/HomePage.jsx
@@ -1,10 +1,12 @@
+import { Box } from "@mui/material";
 import React from "react";
+import Navbar from "scenes/navbar/Navbar";
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>HOme Page</h1>
-    </div>
+    <Box>
+      <Navbar />
+    </Box>
   );
 };
 
