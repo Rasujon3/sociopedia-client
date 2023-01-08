8a8946f35d5d9b465e50677e9a04fc571aa81439
Rasujon3 <rasujon3@gmail.com>
2023-01-08 21:43:59 +0600

    posts widget done

--- src/scenes/homePage/HomePage.jsx
+++ src/scenes/homePage/HomePage.jsx
@@ -2,7 +2,10 @@ import { Box, useMediaQuery } from "@mui/material";
 import React from "react";
 import { useSelector } from "react-redux";
 import Navbar from "scenes/navbar/Navbar";
+import AdvertWidget from "scenes/widgets/AdvertWidget";
+import FriendListWidget from "scenes/widgets/FriendListWidget";
 import MyPostWidget from "scenes/widgets/MyPostWidget";
+import PostsWidget from "scenes/widgets/PostsWidget";
 import UserWidget from "scenes/widgets/UserWidget";
 
 const HomePage = () => {
@@ -26,13 +29,13 @@ const HomePage = () => {
           mt={isNonMobileScreens ? undefined : "2rem"}
         >
           <MyPostWidget picturePath={picturePath} />
-          {/* <PostsWidget userId={_id} /> */}
+          <PostsWidget userId={_id} />
         </Box>
         {isNonMobileScreens && (
           <Box flexBasis="26%">
-            {/* <AdvertWidget /> */}
+            <AdvertWidget />
             <Box m="2rem 0" />
-            {/* <FriendListWidget userId={_id} /> */}
+            <FriendListWidget userId={_id} />
           </Box>
         )}
       </Box>
--- src/state/index.js
+++ src/state/index.js
@@ -34,11 +34,8 @@ export const authSlice = createSlice({
     },
     setPost: (state, action) => {
       const updatedPosts = state.posts.map((post) => {
-        if (post._id === action.payload.post_id) {
-          return action.payload.post;
-        } else {
-          return post;
-        }
+        if (post._id === action.payload.post._id) return action.payload.post;
+        return post;
       });
       state.posts = updatedPosts;
     },
